2faca362971d9c83db6f20b4a876f09efb18fb94
zlclarrypage <845109501@qq.com>
2022-12-02 23:22:59 +0800

    first commit

---
+++ src/main/java/com/rise/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.rise.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张牧之
+ * @date 2022年12月02日 11:17 PM
+ * @Email lichang.zhang@jgdt.com
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+        System.out.println("GatewayApplication start success.");
+    }
+}
